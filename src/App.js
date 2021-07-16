c0d6346a222d3795594a2a0b03613b1830372609
Berkay Baltas <berkay@Berkays-MacBook-Pro.local>
2021-07-15 22:37:43 -0400

    fix navbar and form 3

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Home from "./Home.js";
 import Contact from "./Contact";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import AboutMe from "./AboutMe";
+import ScrollToTop from "./ScrollToTop";
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/Contact" component={Contact} />
