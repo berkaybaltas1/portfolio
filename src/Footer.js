debfade1660490718f1d806f1b4c2a184b923df3
Berkay Baltas <berkaybaltas@gmail.com>
2022-07-31 17:42:20 -0500

    add rel to footer

--- src/Footer.js
+++ src/Footer.js
@@ -8,19 +8,31 @@ function Footer() {
       <div className='footer-list-container'>
         <ul>
           <li>
-            <a href='https://www.instagram.com/berkay.baltas/' target='_blank'>
+            <a
+              href='https://www.instagram.com/berkay.baltas/'
+              target='_blank'
+              rel='noreferrer'
+            >
               <RiInstagramFill className='m-2 footer-icon' />
               Instagram
             </a>
           </li>
           <li>
-            <a href='https://twitter.com/berkaybaltas1' target='_blank'>
+            <a
+              href='https://twitter.com/berkaybaltas1'
+              target='_blank'
+              rel='noreferrer'
+            >
               <RiTwitterFill className='m-2 footer-icon' />
               Twitter
             </a>
           </li>
           <li>
-            <a href='https://github.com/berkaybaltas1' target='_blank'>
+            <a
+              href='https://github.com/berkaybaltas1'
+              target='_blank'
+              rel='noreferrer'
+            >
               <RiGithubFill className='m-2 footer-icon' />
               Github
             </a>
