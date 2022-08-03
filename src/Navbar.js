ca86feb82f107ab471e03296becc03d6a32899ed
Berkay Baltas <berkaybaltas@gmail.com>
2022-08-02 21:56:13 -0500

    fix padding in menu

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,43 +1,44 @@
-import React, { useState } from "react";
-import { HiMenu, HiOutlineX } from "react-icons/hi";
-import "./css/Navbar.css";
-import logo from "./images/bold.png";
-import { useHistory } from "react-router-dom";
+import React, { useState } from 'react'
+import { HiMenu, HiOutlineX } from 'react-icons/hi'
+import './css/Navbar.css'
+import logo from './images/bold.png'
+import { useHistory } from 'react-router-dom'
 
 function Navbar() {
-  const [clicked, setClicked] = useState(false);
-  let history = useHistory();
+  const [clicked, setClicked] = useState(false)
+  let history = useHistory()
   return (
-    <nav className="navbar-container">
-      <div className="nav-menu" onClick={() => setClicked(!clicked)}>
+    <nav className='navbar-container'>
+      <div className='nav-menu' onClick={() => setClicked(!clicked)}>
         <img
-          onClick={() => history.push("/")}
-          className="logo"
+          onClick={() => history.push('/')}
+          className='logo'
           src={logo}
-          alt="logo"
+          alt='logo'
         ></img>
         <i>
           {clicked ? (
-            <HiOutlineX className="icon-menu-close" />
+            <HiOutlineX className='icon-menu-close' />
           ) : (
-            <HiMenu className="icon-menu-open" />
+            <HiMenu className='icon-menu-open' />
           )}
         </i>
-
-        <ul className={clicked ? "menu-open" : "menu-close"}>
-          <li class="active">
-            <button onClick={() => history.push("/")}>Home</button>
-          </li>
-          <li>
-            <button onClick={() => history.push("/about")}>About me</button>
-          </li>
-          <li>
-            <button onClick={() => history.push("/contact")}>Contact</button>
-          </li>
-        </ul>
+        <div className={clicked ? 'menu-open' : 'menu-close'}>
+          <ul>
+            <li class='active'>
+              <button onClick={() => history.push('/')}>Home</button>
+            </li>
+            <li>
+              <button onClick={() => history.push('/about')}>About me</button>
+            </li>
+            <li>
+              <button onClick={() => history.push('/contact')}>Contact</button>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
