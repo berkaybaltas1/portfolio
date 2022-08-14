b257c0b33b2507ed731c5047eda6b8a07681a45f
Berkay Baltas <berkaybaltas@gmail.com>
2022-08-13 22:26:59 -0500

    update profile picture

--- src/Section.js
+++ src/Section.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './css/Section.css'
-import profile from './images/profile-pic.jpeg'
+import profile from './images/personal-pic.png'
 
 function Section() {
   return (
